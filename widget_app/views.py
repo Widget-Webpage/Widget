ef3358ef2fcbddcd0314928af9958fec2c23a384
Arun Annamalai <arunannamalai@Aruns-MacBook-Pro.local>
2021-01-12 18:05:15 -0500

    Tried to get some modal stuff working, still a work in progress

--- widget_app/views.py
+++ widget_app/views.py
@@ -8,7 +8,11 @@ def weather_widget(request):
     return render(request, "weather.html")
 
 def empty_canvas(request):
-    return render(request, "empty_canvas.html")
+    context = {'twitter': ["T", "w", "P"],
+               'stocks': "S",
+               'weather': ["weather_widget1", "weather_widget2"],
+               'sample_int': 7}
+    return render(request, "empty_canvas.html", context)
 
 def empty_canvas2(request):
     return render(request, "empty_canvas2.html")
